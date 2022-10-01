57fdf4c576fdedcdf8134709d7639d123e189e13
Pullusb <bernou.samuel@gmail.com>
2022-10-01 22:09:46 +0200

    optional 2.7 update terms and quoted match
    1.4.0
    
    - added: checkbox to separate specific terms related to upgrade from 2.7
    - added: 'quoted' method to match term only if it's quoted (usefull for icons)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+1.4.0 - 2022-10-01
+
+- added: checkbox to separate specific terms related to upgrade from 2.7
+- added: 'quoted' method to match term only if it's quoted (usefull for icons)
+
 1.3.0 - 2022-10-01
 
 - changed: converted to multifile (`__init__`) addon 
--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
 '''
-Original authors notes:
+authors notes:
 name: update_check_280.py
 author: nBurn
 description: Simple script to help prep 2.7x Blender addons for 2.80
 version: 0, 1, 2
 first released: 2019-09-08
 UI added: 2019-10-26 by tin2tin
+Updated 2019/2022 by Pullusb
 
 LICENSE (MIT)
 
@@ -36,7 +37,7 @@ this Software without prior written authorization.
 bl_info = {
     "name": "Script Update Checker",
     "author": "nBurn, tin2tin, Pullusb",
-    "version": (1, 3, 0),
+    "version": (1, 4, 0),
     "blender": (3, 3, 0),
     "location": "Text Editor > Sidebar > Text > Update Script",
     "description": "Runs  on current document",
@@ -45,136 +46,21 @@ bl_info = {
     "category": "Text Editor",
 }
 
-TERMS = (
-    ("color", "need alpha?"),
-    ("viewnumpad", "view_axis"),
-    ("get_rna", "get_rna_type"),
-    ("viewport_shade", "shading.type"),
-    ("TOOLS", "UI"),
-    ("ZOOMIN", "ADD"),
-    ("ZOOMOUT", "REMOVE"),
-    ("NEW", "FILE_NEW"),
-    ("BBOX", "SHADING_BBOX"),
-    ("POTATO", "SHADING_TEXTURE"),
-    ("SMOOTH", "SHADING_RENDERED"),
-    ("SOLID", "SHADING_SOLID"),
-    ("WIRE", "SHADING_WIRE"),
-    ("ORTHO", "XRAY"),
-    ("BUTS", "PROPERTIES"),
-    ("IMAGE_COL", "IMAGE"),
-    ("OOPS", "OUTLINER"),
-    ("IPO", "GRAPH"),
-    ("SCRIPTWIN", "PREFERENCES"),
-    ("CURSOR", "PIVOT_CURSOR"),
-    ("ROTATECOLLECTION", "PIVOT_INDIVIDUAL"),
-    ("ROTATECENTER", "PIVOT_MEDIAN"),
-    ("ROTACTIVE", "PIVOT_ACTIVE"),
-    ("FULLSCREEN", "WINDOW"),
-    ("LAMP_DATA", "LIGHT_DATA"),
-    ("OUTLINER_OB_LAMP", "OUTLINER_OB_LIGHT"),
-    ("OUTLINER_DATA_LAMP", "OUTLINER_DATA_LIGHT"),
-    ("LAMP_POINT", "LIGHT_POINT"),
-    ("LAMP_SUN", "LIGHT_SUN"),
-    ("LAMP_SPOT", "LIGHT_SPOT"),
-    ("LAMP_HEMI", "LIGHT_HEMI"),
-    ("LAMP_AREA", "LIGHT_AREA"),
-    ("LAMP", "LIGHT"),
-    ("VISIBLE_IPO_ON", "HIDE_OFF"),
-    ("VISIBLE_IPO_OFF", "HIDE_ON"),
-    ("LINK_AREA", "LINKED"),  #removed
-    ("PLUG", "PLUGIN"),  #removed
-    ("LINK", "DOT"),  #removed
-    ("ORTHO", "VIEW_ORTHO"),  #removed
-    ("GAME", "Icon missing. Replace it."),
-    ("DOTSUP", "Icon missing. Replace it."),
-    ("DOTSDOWN", "Icon missing. Replace it."),
-    ("INLINK", "Icon missing. Replace it."),
-    ("EDIT", "Icon missing. Replace it."),
-    ("RADIO", "Icon missing. Replace it."),
-    ("GO_LEFT", "Icon missing. Replace it."),
-    ("TEMPERATURE", "Icon missing. Replace it."),
-    ("SNAP_SURFACE", "Icon missing. Replace it."),
-    ("MANIPUL", "Icon missing. Replace it."),
-    ("BORDER_LASSO", "Icon missing. Replace it."),
-    ("MAN_TRANS", "Icon missing. Replace it."),
-    ("MAN_ROT", "Icon missing. Replace it."),
-    ("MAN_SCALE", "Icon missing. Replace it."),
-    ("RENDER_REGION", "Icon missing. Replace it."),
-    ("RECOVER_AUTO", "Icon missing. Replace it."),
-    ("SAVE_COPY", "Icon missing. Replace it."),
-    ("OPEN_RECENT", "Icon missing. Replace it."),
-    ("LOAD_FACTORY", "Icon missing. Replace it."),
-    ("ALIGN", "Icon missing. Replace it."),
-    ("SPACE2", "Icon missing. Replace it."),
-    ("ROTATE", "Icon missing. Replace it."),
-    ("SAVE_AS", "Icon missing. Replace it."),
-    ("BORDER_RECT", "Icon missing. Replace it."),
-    ("ROTACTIVE", "Icon missing. Replace it."),
-    ("Lamp", "light"),
-    ("lamp", "light"),
-    (".select", "obj.select_set()"),
-    ("backdrop_", "backdrop_offset"),
-    ("tessface", "loop_triangles"),
-    ("user_preferences", "preferences"),
-    (".ops.delete", "context="),
-    #("evaluated_get", "foo"),
-    ("evaluated_depsgraph_get", "evaluated_get"),
-    ("data.meshes.remove", "to_mesh_clear"),
-    ("scene.objects.active", "context.active_object",
-     "context.view_layer.objects.active"),
+## direct word:
+# suggestion for first and second element in tuples
+
+## if first element is 'regex.quoted':
+# second and third element are still direct words but only matched if quoted in text
+
+## if first element is 'regex.sub':
+# second element is a regex pattern
+# itwill be use to search and 'sub' the whole match with the string passed as third
+# captured groups, ex: \g<1> ,can be used to transfer elements from matched string.
+
+TERMS = [
+
     #("scene.object_bases", "view_layer.objects.active"),
-    ("scene.frame_set", "subframe"),
-    (".proportional_edit", "use_proportional_edit"),
-    ("proportional", "use_proportional_edit"),
-    (".label(", ".label(text="),
-    (".label", "align, text (keywords)"),
-    (".row(True)", ".row(align=True)"),
-    (".row(False)", ".row(align=False)"),
-    #    ("row", "align (keyword)"),
-    ("show_x_ray", "show_in_front"),
-    ("popup_menu", "title (keyword)"),
-    (".operator", "text (keyword)"),
-    ("object.hide", "object.hide_viewport"),
-    (".Group(", ".Collection("),
-    (".groups", ".collections"),
-    ("dupli_group", "instance_collection"),
-    (".link(", "active_layer_collection"),
-    ("scene.objects.unlink(", "collection.objects.unlink("),
-    (".draw_type", ".display_type"),
-    (".draw_size", ".display_size"),
-    ("uv_textures.", "uv_layers."),
-    (".transform_apply", "scale="),
-    ("view_align", "align='WORLD'"),
-    ("mesh.primitive", "size, layers"),
-    ("transform_orientation", "transform_orientation_slots"),
-    ("constraint_orientation", "orient_type"),
-    ("show_manipulator", "show_gizmo"),
-    ("use_weight_color_range", "view.use_weight_color_rang"),
-    ("wm.addon_", "preferences"),
-    ("percentage", "factor"),
-    ("use_x", "use_axis"),
-    ("use_y", "use_axis"),
-    ("use_z", "use_axis"),
-    ("scene_update_pre", "depsgraph_update_pre"),
-    ("scene_update_post", "depsgraph_update_post"),
-    ("scene.update", "view_layer.update()"),
-    ("use_occlude_geometry", "shading.show_xray"),
-    ("event_timer_add", "time_step= (keyword)"),
-    ("frame_set", "subframe= (keyword)"),
     ("INFO_MT_", "TOPBAR_MT_"),
-    ("_specials", "_context_menu"),
-    ("basis", "noise_basis= (keyword)"),
-    ("turbulence_vector", "noise_basis= (keyword)"),
-    ("tweak_threshold", "drag_threshold"),
-    ("cursor_location", "cursor.location"),
-    ("snap_element", "snap_elements"),
-    (".pivot_point", "transform_pivot_point"),
-    ("header_text_set", "_set(None)"),
-    ("    bpy.utils.register_module(__name__)", """    for i in classes:
-        bpy.utils.register_class(i)"""),
-    ("    bpy.utils.unregister_module(__name__)", """    for i in classes:
-        bpy.utils.unregister_class(i)"""),
-    ("register_module", "register_class"),
     #("bl_idname", "only needed for Operator"),
     #("Property", ": (annotation)"),
     (" = StringProperty(", ": StringProperty("),
@@ -199,18 +85,22 @@ TERMS = (
     (" = bpy.props.IntVectorProperty(", ": bpy.props.IntVectorProperty("),
     (" = bpy.props.PointerProperty(", ": bpy.props.PointerProperty("),
     (" = bpy.props.RemoveProperty(", ": bpy.props.RemoveProperty("),
-    (".prop", "text (keyword)"),
-    ("Operator):", "UPPERCASE_OT_snake_case("),
+    # ("Operator):", "UPPERCASE_OT_snake_case("),
+    ### test on OPS (!no good, need conditions to be usefull!)
+    # ('regex.sub',
+    #     r"(?:class )?([a-z]*)_([a-z]*)?(\((?:bpy\.types\.)?Operator\):)",
+    #     lambda m: r'{}_OT_{}{}'.format(m.group(1).upper(), m.group(2), m.group(3))),
+
+    # ("Panel):", "UPPERCASE_PT_snake_case("),
+    # ("Menu):", "UPPERCASE_MT_snake_case("),
+    # ("UIList):", "UPPERCASE_UL_snake_case("),
     #    ("Operator", "_OT"),
-    ("Panel):", "UPPERCASE_PT_snake_case("),
     #    ("Panel", "_PT"),
-    ("Menu):", "UPPERCASE_MT_snake_case("),
     #    ("Menu", "_MT"),
-    ("UIList):", "UPPERCASE_UL_snake_case("),
     #    ("UIList", "_UL"),
-    ("keymap_items", "name=name_arg"),
 
     ## Replace obsolete bgl in 3.4
+    ("import bgl", ""),
     ("bgl.glEnable(bgl.GL_BLEND)", "gpu.state.blend_set('ALPHA')"),
     ("bgl.glDisable(bgl.GL_BLEND)", "gpu.state.blend_set('NONE')"),
     ("gpu.shader.from_builtin('2D_UNIFORM_COLOR')", "gpu.shader.from_builtin('POLYLINE_UNIFORM_COLOR')"),
@@ -222,8 +112,119 @@ TERMS = (
     ('regex.sub', r"bgl\.glPointSize\([a-zA-Z0-9]+\)", """gpu.state.program_point_size_set(False)
     gpu.state.point_size_set(\g<1>)"""),
 
-)
+]
 
+TERMS_27 = [
+    (".prop", "text (keyword)"),
+    # (".label(", ".label(text="),
+    ('regex.sub', ".label\((?!text=)(.*?)\)", ".label(text=\g<1>)"),
+
+    ("viewport_shade", "shading.type"),
+    ("data.meshes.remove", "to_mesh_clear"),
+    ("scene.objects.active", "context.active_object",
+     "context.view_layer.objects.active"),
+    (".draw_type", ".display_type"),
+    (".draw_size", ".display_size"),
+    ("uv_textures.", "uv_layers."),
+    (".transform_apply", "scale="),
+    ("view_align", "align='WORLD'"),
+    # ("color", "need alpha?"),
+    ("keymap_items", "name=name_arg"),
+    ("viewnumpad", "view_axis"),
+    ("get_rna", "get_rna_type"),
+    ("tessface", "loop_triangles"),
+    (".ops.delete", "context="),
+    ("evaluated_depsgraph_get", "evaluated_get"),
+    ("mesh.primitive", "size, layers"),
+    ("transform_orientation", "transform_orientation_slots"),
+    ("constraint_orientation", "orient_type"),
+    ("show_manipulator", "show_gizmo"),
+    ("use_weight_color_range", "view.use_weight_color_rang"),
+    ("percentage", "factor"),
+    ("use_x", "use_axis"),
+    ("use_y", "use_axis"),
+    ("use_z", "use_axis"),
+    ("use_occlude_geometry", "shading.show_xray"),
+    ("event_timer_add", "time_step= (keyword)"),
+    ("_specials", "_context_menu"),
+    ("basis", "noise_basis= (keyword)"),
+    ("turbulence_vector", "noise_basis= (keyword)"),
+    ("dupli_group", "instance_collection"),
+    (".link(", "active_layer_collection"),
+    ("scene.objects.unlink(", "collection.objects.unlink("),
+    ("wm.addon_", "preferences"),
+    ("object.hide", "object.hide_viewport"),
+    (".Group(", ".Collection("),
+    (".groups", ".collections"),
+    ("snap_element", "snap_elements"),
+    ("tweak_threshold", "drag_threshold"),
+    ("cursor_location", "cursor.location"),
+    (".pivot_point", "transform_pivot_point"),
+    ("scene.frame_set", "subframe"),
+    (".proportional_edit", "use_proportional_edit"),
+    ("proportional", "use_proportional_edit"),
+    ("user_preferences", "preferences"),
+    ("scene_update_pre", "depsgraph_update_pre"),
+    ("scene_update_post", "depsgraph_update_post"),
+    ("scene.update", "view_layer.update()"),
+    ("frame_set", "subframe= (keyword)"),
+
+    ## objects
+    # ("Lamp", "light"),
+    # ("lamp", "light"),
+    ("show_x_ray", "show_in_front"),
+    ("popup_menu", "title (keyword)"),
+    (".operator", "text (keyword)"),
+    ('regex.sub', r"\b(L|l)amp(\b|\.)", r"light\2"),
+    (".select", "obj.select_set()"),
+    ("backdrop_", "backdrop_offset"),
+
+    ("header_text_set", "_set(None)"),
+    ## registers
+    ("    bpy.utils.register_module(__name__)", """    for i in classes:
+        bpy.utils.register_class(i)"""),
+    ("    bpy.utils.unregister_module(__name__)", """    for i in classes:
+        bpy.utils.unregister_class(i)"""),
+    ("register_module", "register_class"),
+    
+    ## old icons
+    ('regex.quoted', "TOOLS", "UI"), 
+    # equivalent to # ('regex.sub', r"('|\")TOOLS(\1)", "\g<1>UI\g<2>"),
+    ('regex.quoted', "ZOOMIN", "ADD"),
+    ('regex.quoted', "ZOOMOUT", "REMOVE"),
+    ('regex.quoted', "NEW", "FILE_NEW"),
+    ('regex.quoted', "BBOX", "SHADING_BBOX"),
+    ('regex.quoted', "POTATO", "SHADING_TEXTURE"),
+    ('regex.quoted', "SMOOTH", "SHADING_RENDERED"),
+    ('regex.quoted', "SOLID", "SHADING_SOLID"),
+    ('regex.quoted', "WIRE", "SHADING_WIRE"),
+    ('regex.quoted', "ORTHO", "XRAY"),
+    ('regex.quoted', "BUTS", "PROPERTIES"),
+    ('regex.quoted', "IMAGE_COL", "IMAGE"),
+    ('regex.quoted', "OOPS", "OUTLINER"),
+    ('regex.quoted', "IPO", "GRAPH"),
+    ('regex.quoted', "SCRIPTWIN", "PREFERENCES"),
+    ('regex.quoted', "CURSOR", "PIVOT_CURSOR"),
+    ('regex.quoted', "ROTATECOLLECTION", "PIVOT_INDIVIDUAL"),
+    ('regex.quoted', "ROTATECENTER", "PIVOT_MEDIAN"),
+    ('regex.quoted', "ROTACTIVE", "PIVOT_ACTIVE"),
+    ('regex.quoted', "FULLSCREEN", "WINDOW"),
+    ('regex.quoted', "LAMP_DATA", "LIGHT_DATA"),
+    ('regex.quoted', "OUTLINER_OB_LAMP", "OUTLINER_OB_LIGHT"),
+    ('regex.quoted', "OUTLINER_DATA_LAMP", "OUTLINER_DATA_LIGHT"),
+    ('regex.quoted', "LAMP_POINT", "LIGHT_POINT"),
+    ('regex.quoted', "LAMP_SUN", "LIGHT_SUN"),
+    ('regex.quoted', "LAMP_SPOT", "LIGHT_SPOT"),
+    ('regex.quoted', "LAMP_HEMI", "LIGHT_HEMI"),
+    ('regex.quoted', "LAMP_AREA", "LIGHT_AREA"),
+    ('regex.quoted', "LAMP", "LIGHT"),
+    ('regex.quoted', "VISIBLE_IPO_ON", "HIDE_OFF"),
+    ('regex.quoted', "VISIBLE_IPO_OFF", "HIDE_ON"),
+    ('regex.quoted', "LINK_AREA", "LINKED"),  #removed
+    ('regex.quoted', "PLUG", "PLUGIN"),  #removed
+    ('regex.quoted', "LINK", "DOT"),  #removed
+    ('regex.quoted', "ORTHO", "VIEW_ORTHO"),  #removed
+]
 #terms = str(TERMS).split('\n')
 #for t in terms:
 #    print('("' + t + '", ' + '"foo"),')
@@ -253,18 +254,31 @@ def check_files(txt):
                 # break
             
             # Check for Terms
-            for t in TERMS:
+            if bpy.context.scene.check_27:
+                # add terms relative to upgrade from 2.7
+                term_list = TERMS + TERMS_27
+            else:
+                term_list = TERMS
+
+            for t in term_list:
                 if t[0].startswith('regex.'):
                     pattern, repl = t[1], t[2]
                     
-                    res = re.search(pattern, line):
+                    if t[0].split('.')[-1] == 'quoted':
+                        # Case where Word must be surrounded by quotes to be valid
+                        # pattern and repl are actually raw words
+                        pattern = r"('|\")" + pattern + r"(\1)"
+                        repl = r"\g<1>" + repl + r"\g<2>"
+                    
+                    res = re.search(pattern, line)
                     if not res:
                         continue
 
-                    if t[0].endswith('sub'):
+                    if t[0].split('.')[-1] in ('sub', 'quoted'):
                         string = res.group(0)
                         suggestion = re.sub(pattern, repl, string)
                         classes.append([int(i), line, string, suggestion])
+                    
 
                 else:
                     if t[0] in line:
@@ -350,7 +364,10 @@ class TEXT_PT_show_update_script(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         st = context.space_data
+
+        layout.prop(context.scene, 'check_27', text='Include Terms From Blender 2.7')
         layout.operator("text.update_script_button")
+        
         box = layout.box()
 
         if bpy.types.Scene.update_script_name == bpy.context.space_data.text.filepath:
@@ -412,19 +429,22 @@ classes = (
 
 
 def register():
-
-    for i in classes:
-        bpy.utils.register_class(i)
+    bpy.types.Scene.check_27 = bpy.props.BoolProperty(
+        name='Include 2.7 Terms', default=True,
+        description='search for terms specific to blender 2.7 version')
     bpy.types.Scene.update_script = bpy.props.StringProperty()
     bpy.types.Scene.update_script_name = bpy.props.StringProperty()
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
 
 def unregister():
 
-    for i in classes:
-        bpy.utils.unregister_class(i)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     del bpy.types.Scene.update_script
     del bpy.types.Scene.update_script_name
+    del bpy.types.Scene.check_27
 
 
 if __name__ == "__main__":
